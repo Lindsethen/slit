7b89af1349a916faec2a1030e7e81f538ec38ded
Lindsethen <henrik.lindseth1@gmail.com>
2018-11-24 19:38:57 +0100

    Listermoduler

--- src/java/ModuleUtil/ListModules.java
+++ src/java/ModuleUtil/ListModules.java
@@ -5,10 +5,42 @@
  */
 package ModuleUtil;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /**
  *
- * @author matjo
+ * @author henlind
  */
+
 public class ListModules {
-    
+        private Connection conn = null;
+        private Statement stmt = null;
+        private ResultSet rs = null;
+    public void selectAll(){
+    try {
+        String query = "SELECT * FROM MODULE;";
+        conn = DbUtil.ConnectionManager.getConnection();
+        stmt = conn.createStatement();
+        rs = stmt.executeQuery(query);
+            
+      // iterate through the java resultset
+      while (rs.next())
+      {
+        int id = rs.getInt("u_id");
+        String mName = rs.getString("u_firstName");
+        String mDesc = rs.getString("u_lastName");
+        
+        // print the results
+        System.out.format("%s, %s, %s\n", id, mName, mDesc);
+      }
+         stmt.close();
+            }
+            catch (Exception e)
+            {
+              System.err.println("Got an exception! ");
+              System.err.println(e.getMessage());
+            }
+  }
 }
