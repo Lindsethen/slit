2b95d92551d5b7499df113ae0d5b5fabf6177efa
mariusjt <matjot@gmail.com>
2018-11-25 15:30:47 +0100

    Fikset login.java

--- src/java/Systemlogin/Login.java
+++ src/java/Systemlogin/Login.java
@@ -21,20 +21,32 @@ import javax.servlet.http.HttpSession;
 @WebServlet(name ="LoginServlet")
 public class Login extends HttpServlet {
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    
+    
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-              String uname = request.getParameter("uname");
-              String pass = request.getParameter("pass");
 
-              if(uname.equals("student") && pass.equals("student"))
-              {
-                  HttpSession session = request.getSession();
-                  session.setAttribute("username", uname);
-                  response.sendRedirect("welcome.jsp");
-              }
-              else
-              {
-                  response.sendRedirect("login.jsp");
-              }
+            String uname = request.getParameter("uname");
+            String pass = request.getParameter("pass");
+
+            if(uname.equals("telusko") && pass.equals("learnings"))
+            {
+
+                HttpSession session = request.getSession();
+                session.setAttribute("username", uname);
+                response.sendRedirect("welcome.jsp");
+
+            }
+            else
+            {
+                if(uname.equals("student") && pass.equals("123")) {
+                    HttpSession session = request.getSession();
+                    session.setAttribute("username", uname);
+                    response.sendRedirect("welcome.jsp");
+                } else {
+                    response.sendRedirect("login.jsp");
+                }
+                }
     }
 }
