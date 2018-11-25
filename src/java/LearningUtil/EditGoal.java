f6ccedb3287a6a7d85a8e43d0b8d825dfbd600e3
Kristian <awesome.krydder@gmail.com>
2018-11-25 14:48:34 +0100

    lagde editgoals og la til mer på edit user

--- src/java/LearningUtil/EditGoal.java
+++ src/java/LearningUtil/EditGoal.java
@@ -4,11 +4,47 @@
  * and open the template in the editor.
  */
 package LearningUtil;
+import java.sql.*;
 
 /**
  *
- * @author matjo
+ * @author Ludamac
  */
 public class EditGoal {
+        private Connection conn = null;
+        private Statement stmt = null;
+        String newName;
+        String newString;
+        String sqlQuery;
+        int goalID;
         
+public void changeGoalname(String nameString, int goalID){
+    newName = nameString;
+    goalID = this.goalID;
+    sqlQuery = "UPDATE LEARNINGGOAL SET lg_name = " + newName + "WHERE lg_id = " + goalID;
+    try{
+        conn = DbUtil.ConnectionManager.getConnection();
+        stmt = conn.createStatement();
+        stmt.executeUpdate(sqlQuery);
+    }
+    catch (SQLException ex){
+        System.out.println("Det er noe som ikke stemmer her :(");
+        System.out.println(ex);
+    }
+}
+public void changeGoalstring(String stringString, int goalID){
+    newString = stringString;
+    goalID = this.goalID;
+    sqlQuery = "UPDATE LEARNINGGOAL SET lg_string = " + newString + "WHERE lg_id = " + goalID;
+    try{
+        conn = DbUtil.ConnectionManager.getConnection();
+        stmt = conn.createStatement();
+        stmt.executeUpdate(sqlQuery);
+    }
+    catch (SQLException ex){
+        System.out.println("Det er noe som ikke stemmer her :(");
+        System.out.println(ex);
+    }
+}
+                
 }
