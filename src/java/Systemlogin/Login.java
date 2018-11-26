dc22455adc140c9ce742835a003202f93f1d0eb6
mariusjt <matjot@gmail.com>
2018-11-26 16:33:23 +0100

    Fikset brukernavn/passord på teacher

--- src/java/Systemlogin/Login.java
+++ src/java/Systemlogin/Login.java
@@ -37,7 +37,7 @@ public class Login extends HttpServlet {
             }
             else
             {
-                if(uname.equals("lærer") && pass.equals("lærer")) {
+                if(uname.equals("teacher") && pass.equals("teacher")) {
                     HttpSession session = request.getSession();
                     session.setAttribute("username", uname);
                     response.sendRedirect("Teacher/index.jsp");
