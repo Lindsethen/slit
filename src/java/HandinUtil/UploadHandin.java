6a043bda28318b4e3f01ae5a4ac0f42ce25182ed
Lindsethen <henrik.lindseth1@gmail.com>
2018-11-26 13:47:46 +0100

    handin

--- src/java/HandinUtil/UploadHandin.java
+++ src/java/HandinUtil/UploadHandin.java
@@ -5,10 +5,91 @@
  */
 package HandinUtil;
 
-/**
- *
- * @author Henrik
- */
-public class UploadHandin {
-    
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+ 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+ 
+@WebServlet("/uploadServlet")
+@MultipartConfig(maxFileSize = 16177215)    // upload file's size up to 16MB
+public class UploadHandin extends HttpServlet {
+     
+    // database connection settings
+    private String dbURL = "jdbc:mysql://localhost:3306/AppDB";
+    private String dbUser = "root";
+    private String dbPass = "secret";
+     
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+        // gets values of text fields
+        String studentName = request.getParameter("studentName");
+        String moduleNumber = request.getParameter("muduleNumber");
+         
+        InputStream inputStream = null; // input stream of the upload file
+         
+        // obtains the upload file part in this multipart request
+        Part filePart = request.getPart("handinHere");
+        if (filePart != null) {
+            // prints out some information for debugging
+            System.out.println(filePart.getName());
+            System.out.println(filePart.getSize());
+            System.out.println(filePart.getContentType());
+             
+            // obtains input stream of the upload file
+            inputStream = filePart.getInputStream();
+        }
+         
+        Connection conn = null; // connection to the database
+        String message = null;  // message will be sent back to client
+         
+        try {
+            // connects to the database
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            conn = DriverManager.getConnection(dbURL, dbUser, dbPass);
+ 
+            // constructs SQL statement
+            String sql = "INSERT INTO HANDIN (studentName, moduleNumber, handinHere) values (?, ?, ?)";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, studentName);
+            statement.setString(2, moduleNumber);
+             
+            if (inputStream != null) {
+                // fetches input stream of the upload file for the blob column
+                statement.setBlob(3, inputStream);
+            }
+ 
+            // sends the statement to the database server
+            int row = statement.executeUpdate();
+            if (row > 0) {
+                message = "File uploaded and saved into database";
+            }
+        } catch (SQLException ex) {
+            message = "ERROR: " + ex.getMessage();
+            ex.printStackTrace();
+        } finally {
+            if (conn != null) {
+                // closes the database connection
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+            // sets the message in request scope
+            request.setAttribute("Message", message);
+             
+            // forwards to the message page
+            getServletContext().getRequestDispatcher("/Message.jsp").forward(request, response);
+        }
+    }
 }
---
+++ web/UploadHandin.jsp
@@ -0,0 +1,41 @@
+<%-- 
+    Document   : UploadHandin
+    Created on : 26.nov.2018, 12:31:12
+    Author     : Henrik
+--%>
+
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//DELIVER HAND-IN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>File Upload to Database example</title>
+</head>
+<body>
+    <center>
+        <h1>File Upload to Database example</h1>
+        <form method="post" action="uploadServlet" enctype="multipart/form-data">
+            <table border="0">
+                <tr>
+                    <td>Student: </td>
+                    <td><input type="text" name="studentName" size="50"/></td>
+                </tr>
+                <tr>
+                    <td>Module: </td>
+                    <td><input type="text" name="moduleNumber" size="50"/></td>
+                </tr>
+                <tr>
+                    <td>:Upload </td>
+                    <td><input type="file" name="handinHere" size="50"/></td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <input type="submit" value="Save">
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </center>
+</body>
+</html>
