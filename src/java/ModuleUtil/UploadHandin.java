e3f4d24ee7b898603e835c8fce71aa65d63cbb4e
Lindsethen <henrik.lindseth1@gmail.com>
2018-11-26 11:01:40 +0100

    Handin

---
+++ src/java/ModuleUtil/UploadHandin.java
@@ -0,0 +1,27 @@
+/*
+ * Upload 
+ */
+package ModuleUtil;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+ 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+
+/**
+ *
+ * @author Henlind
+ */
+public class UploadHandin extends HttpServlet{
+    
+}
